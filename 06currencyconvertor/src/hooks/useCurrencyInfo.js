58d33e6572f193dc53f1dec7b27e21f6030da1d5
yashgwala321 <yashgwala4@gmail.com>
2026-05-17 00:29:06 +0530

    feat: currency converter with live API and InputBox component

--- 06currencyconvertor/src/hooks/useCurrencyInfo.js
+++ 06currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -1,14 +1,14 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
-function useCurrencyInfo(currency){
-    useEffect( () => {
-        const [data ,setData] = useState({})
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
-        .then((res) => res.json())
-        .then((res) => setData(res[currency]))
-        console.table(data)
-    } , [currency])
-    console.log(data)
+function useCurrencyInfo(currency) {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch(`https://latest.currency-api.pages.dev/v1/currencies/${currency}.json`)
+            .then((res) => res.json())
+            .then((res) => setData(res[currency]))
+    }, [currency])
+
     return data
 }
 
