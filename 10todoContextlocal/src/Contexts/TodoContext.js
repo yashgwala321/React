f51d96141877bfdecc04ca539da302bbf30cdeab
yashgwala321 <yashgwala4@gmail.com>
2026-05-20 17:41:04 +0530

    Context API Add

--- 10todoContextlocal/src/Contexts/TodoContext.js
+++ 10todoContextlocal/src/Contexts/TodoContext.js
@@ -1,7 +1,19 @@
 import React from "react";
 import { useContext ,createContext } from "react";
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos:[
+        {
+            id: 1,
+            todo: "Todo Msg",
+            Comleted: false,
+        }
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo : (id) => {},
+    toggleComplete : (id) => {}
+})
 
 export const useTodo = () => {
     return useContext(TodoContext)
