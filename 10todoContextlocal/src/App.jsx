abd9570f5e94ffcbe5b7538e9db57a20f5e68374
yashgwala321 <yashgwala4@gmail.com>
2026-05-20 23:56:14 +0530

    Done Todo Project

--- 10todoContextlocal/src/App.jsx
+++ 10todoContextlocal/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { TodoProvider } from './Contexts/TodoContext'
+import { TodoForm, TodoItem } from './Components'
 
 function App() {
 
@@ -26,23 +27,29 @@ function App() {
       if(todos && todos.length > 0){
         setTodos(todos)
       }
-    })
+    },[])
 
     useEffect(() => {
       localStorage.setItem("todos" , JSON.stringify(todos))
     }, [todos])
 
-    
+
   return(
     <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
       <div className="bg-[#172842] min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
-                        {/* Todo form goes here */} 
+                        <TodoForm /> 
                     </div>
                     <div className="flex flex-wrap gap-y-3">
                         {/*Loop and Add TodoItem here */}
+                        {todos.map((todo) => (
+                          <div key={todo.id}
+                          className='w-full'>
+                             <TodoItem todo={todo}/>
+                          </div>
+                        ))}
                     </div>
                 </div>
             </div>
